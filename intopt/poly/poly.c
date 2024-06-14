56e58185267eb817bf833bcee0015f010b4473f8
Personal Name <elliot.beskow@gmail.com>
2024-06-14 12:07:16 +0200

    rpn + poly

--- intopt/poly/poly.c
+++ intopt/poly/poly.c
@@ -2,6 +2,152 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
 #include "error.h"
 #include "poly.h"
+
+struct poly_t {
+	int* coefficients;
+	int* exponents;
+	int size;
+};
+
+poly_t* new_poly_from_string(const char* s) {
+	int c, j;
+	int i = 0;
+	int size = 1;
+	do {
+		if (s[i] == '+' || s[i] == '-')
+			size++;
+		i++;
+	} while (s[i] != '\0');
+	i = 0;
+	j = 0;
+	int* coefficients = calloc(size, sizeof(int));
+	int* exponents = calloc(size, sizeof(int));
+	int* coefficient = NULL;
+	int* exponent = NULL;
+	int sign = 1;
+	do {
+		c = s[i];
+		if (isdigit(c)) {
+			int num = 0;
+			while (isdigit(c)) {
+				num *= 10;
+				num += c - '0';
+				i++;
+				c = s[i];
+			}
+			if (coefficient == NULL) {
+				coefficient = calloc(1, sizeof(int));
+				*coefficient = num;
+			} else {
+				exponent = calloc(1, sizeof(int));
+				*exponent = num;
+			}
+			continue;
+		} else if (c == '^') {
+			if (coefficient == NULL) {
+				coefficient = calloc(1, sizeof(int));
+				*coefficient = 1;
+			}
+		} else if (c == '+' || c == '-') {
+			coefficients[j] = sign*(*coefficient);
+			if (exponent == NULL)
+				exponents[j] = 1;
+			else
+				exponents[j] = *exponent;
+			free(coefficient);
+			free(exponent);
+			coefficient = NULL;
+			exponent = NULL;
+			if (c == '+')
+				sign = 1;
+			else
+				sign = -1;
+			j++;
+		}
+		i++;
+	} while (c != '\0');
+	coefficients[j] = sign*(*coefficient);
+	free(coefficient);
+	if (exponent == NULL)
+		exponents[j] = 0;
+	else {
+		exponents[j] = *exponent;
+		free(exponent);
+	}
+	poly_t* p = calloc(1, sizeof(poly_t));
+	p->exponents = exponents;
+	p->coefficients = coefficients;
+	p->size = size;
+	return p;
+}
+
+void free_poly(poly_t* p) {
+	free(p->coefficients);
+	free(p->exponents);
+	free(p);	
+}
+
+// poly_t* mul(poly_t* a, poly_t* b) {
+// 	int i, j, k, l;
+// 	int size = a->size * b->size; // max size
+// 	int* coefficients = calloc(size, sizeof(int));
+// 	int* exponents = calloc(size, sizeof(int));
+// 	for (i=0; i<a->size; i++)
+// 		for (j=0; j<b->size; j++) {
+// 			k = i*a->size + j;
+// 			int exponent = a->exponents[k] + b->exponents[k];
+// 			int coefficient = a->coefficients[k] * b->coefficients[k];
+// 			for(l=0; l<k; l++) {
+// 				if (exponents[l] == exponent) {
+// 					k = l;
+// 					break;
+// 				}
+// 			}
+// 			exponents[k] = exponent;
+// 			coefficients[k] += coefficient;
+// 		}
+// 	poly_t* p = malloc(sizeof(poly_t));
+// 	p->coefficients = coefficients;
+// 	p->exponents = exponents;
+// 	p->size = size;
+// 	return p;
+// }
+
+poly_t* mul(poly_t* p, poly_t* q) {
+	int i, j, k;
+	k = 0;
+	for (i=0; i<p->size, i++) {
+		for (j=0; j<q->size, j++) {
+			
+		}
+	}
+}
+
+void print_poly(poly_t* p) {
+	int i;
+	for (i=0; i<p->size; i++) {
+		int coeff = p->coefficients[i];
+		int exp = p->exponents[i];
+		if (coeff!=0) {
+			if (i>0) {
+				if (coeff > 0) {
+					printf(" + ");
+				} else {
+					printf(" - ");
+				}
+			}
+			if (coeff != 1 || exp == 0)
+				printf("%d", abs(coeff));
+			if (exp==1) {
+				printf("x");
+			} else if (exp > 1) {
+				printf("x^%d", exp);
+			}
+		}
+	}	
+	printf("\n");
+}
--- intopt/rpn/rpn.c
+++ intopt/rpn/rpn.c
@@ -1,6 +1,77 @@
+#include <stdio.h>
+#include <ctype.h>
 #define N		(10)
+int stack[N];
+int top = -1;
+int line = 1;
+
+int skipline(int c) {
+	while ((c != '\n') && (c != EOF)) {
+		c = getchar();
+	}
+	top = -1;
+	line++;
+	return c;
+}
 
 int main(void)
 {
-	return 0;
+	int c;
+	do {
+		c = getchar();
+		if (isdigit(c)) {
+			int num = 0;
+			int ch = c;
+			while (isdigit(ch)) {
+				c = ch;
+				num *= 10;
+				num += c - '0';
+				ch = getchar();
+			}
+			if (top == N-1) {
+				printf("line %d: error at %c\n", line, c);
+				c = skipline(ch);
+				continue;
+			}
+			ungetc(ch, stdin);
+			top++;
+			stack[top] = num;
+		//	printf("Added number to stack: %d at position %d\n", num, top);
+		} else if (c == ' ') {
+		} else if (c == '\n') {
+			if (top == 0) {
+				printf("line %d: %d\n", line, stack[0]);
+			} else {
+				printf("line %d: error at \\n\n", line);
+			}
+			line++;
+			top = -1;
+		} else if (c == '+' || c == '-' || c == '*' || c == '/') {
+			if (top < 1) {
+				printf("line %d: error at %c\n", line, c);
+				c = skipline(c);
+			} else {
+				if (c == '+')
+					stack[top-1] = stack[top-1] + stack[top];
+				else if (c == '-')
+					stack[top-1] = stack[top-1] - stack[top];
+				else if (c == '*')
+					stack[top-1] = stack[top-1] * stack[top];
+				else if (c == '/') {
+					if (stack[top] == 0) {
+						printf("line %d: error at /\n", line);
+						c = skipline(c);
+						continue;
+					}
+					stack[top-1] = stack[top-1] / stack[top];
+				}
+				top--;
+				//printf("top=%d, c=%c, stack[top]=%d\n", top, c, stack[top]);
+			}
+		} else if (c == EOF) {} else {
+			printf("line %d: error at %c\n", line, c);
+			c = skipline(c);
+			top = -1;
+		}
+	} while (c != EOF);
 }
